4e9ab26c8d5c9a7a946b30b0bfe65c44af5f4f50
mprbowo <75667039+mprbowo@users.noreply.github.com>
2023-05-08 20:14:05 +0700

    Delete Tugas5 directory

--- Tugas5/AccessingMetadata.py
+++
@@ -1,9 +0,0 @@
-//Accessing metadata information about Hive tables and UDFs is made easy with Catalog API. //Following commands explain how to access metadata. 
-
-spark.catalog.listDatabases().select("name").show()
-
-spark.catalog.listTables.show()
-
-spark.catalog.isCached("sample_07")
-
-spark.catalog.listFunctions().show() 
